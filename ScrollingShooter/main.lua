37265cc77556100076df6b4ca8a40c6045180e3a
Almighty <cylan.insane22@gmail.com>
2015-06-17 03:20:24 -0500

    Edit

--- ScrollingShooter/main.lua
+++ ScrollingShooter/main.lua
@@ -147,6 +147,7 @@ function love.draw(dt)
 
 	for i, enemy in ipairs(enemies) do 
 		love.graphics.draw(enemy.img, enemy.x, enemy.y)
+		love.graphics.rotate( 180 )
 	end
 		
 	for i, bullet in ipairs(bullets) do
