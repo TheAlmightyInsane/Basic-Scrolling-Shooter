9f11a7ec6d98713811f9da71c8a2352ccfe1866b
Almighty <cylan.insane22@gmail.com>
2015-06-17 02:46:52 -0500

    Basic Game

---
+++ ScrollingShooter/conf.lua
@@ -0,0 +1,11 @@
+-- Configuration
+
+function love.conf(t)
+	t.title = "Scrolling Shooter"
+	t.version = "0.9.2"
+	t.window.width = 480
+	t.window.height = 800
+	
+	-- For Windows debugging
+	t.console = true  -- Make sure to disable before giving this to anyone
+end
